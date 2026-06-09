a4a9e3bd1dea8a4b22e9ae3f3f1a6452f9110cdb
Ziming01-Wang <2089548115@qq.com>
2026-06-09 14:59:58 +0800

    key_change

--- client.cpp
+++ client.cpp
@@ -2,6 +2,3 @@
 int main(){
     Client A;
 }
-//ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxKGb0vCXpPrMLf1u196uaoCyLPV0wmvoJiOseXztVF 2089548115@qq.com
-//git remote add origin git@github.com:Ziming01-Wang/Server_z_o.git
-//git push -u origin main
